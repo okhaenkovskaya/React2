a75c332af45c6e3b8c18656d5df3b5c8da4b7ca1
shopifydemoadmn <shopifydemoadmn@gmail.com>
2023-05-04 16:15:00 +0400

    fixed hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,18 +1,17 @@
+import { FC} from 'react';
+
 import style from "./Hero.module.scss";
 import Button from "../Button";
 
-
-const Hero = ({heroData}) => {
-  console.log(heroData, '-------heroData')
-
+const Hero: FC<PageContent.Page> = ({title, button, image}) => {
   return (
     <div
       className={style.hero}
-      style={{ backgroundImage: `url("http://via.placeholder.com/640x360")` }}
+      style={{ backgroundImage: `url(${image.src})` }}
     >
       <div className={style.heroText}>
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        <Button to="/about">Go to page about</Button>
+        <h1>{title}</h1>
+        <Button to={button.url}>{button.title}</Button>
       </div>
     </div>
   );
