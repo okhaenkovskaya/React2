ea4eeea82de78242b356e0da71fe9c59452109ab
shopifydemoadmn <shopifydemoadmn@gmail.com>
2023-05-11 10:55:47 +0400

    develop prompt component

--- src/layout/PublicLayout.tsx
+++ src/layout/PublicLayout.tsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 import { ThemeContext } from "./Contexts.ts";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import PromptScroll from "../components/PromptScroll";
 
 interface PublicLayoutProps {
   children?: React.ReactNode | null;
@@ -16,7 +17,7 @@ function PublicLayout({ children }: PublicLayoutProps) {
     <ThemeContext.Provider value={theme}>
       <div className="page-wrapper">
         <Header setTheme={setTheme} theme={theme} />
-
+        <PromptScroll />
         {children ?? <Outlet />}
 
         <Footer theme={theme} />
